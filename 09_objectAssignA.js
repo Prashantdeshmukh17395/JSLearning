da39e7508d88271133d1b9ae00b18c4dd760b045
Prashantdeshmukh17395 <prashantdeshmukh1233@gmail.com>
2023-03-29 01:06:01 +0530

    object assign A
    object assignment A

---
+++ 09_objectAssignA.js
@@ -0,0 +1,62 @@
+
+let sbiBank = {
+    bankName: "SBI Bank",
+    location: "Badlapur",
+    accountNo: "60131422 ",
+    ifsc: "SBI000481",
+    interestRate: "7.5%",
+    
+    showDetails : function () {
+        console.log(`Name: ${this.bankName}, Location: ${this.location}, Account No.: ${this.accountNo}, IFSC: ${this.ifsc}, Intrest Rate : ${this.interestRate}`);
+        
+    }
+}
+console.log(sbiBank);
+
+
+let showDetails ;
+let axisBank = {
+    bankName: "Axis Bank",
+    location: "Kalyan",
+    accountNo: "30131422 ",
+    ifsc: "AXIS000432",
+    interestRate: "8.0%",
+    
+    showDetails : function () {
+        console.log(`Name: ${this.bankName}, Location: ${this.location}, Account No.: ${this.accountNo}, IFSC: ${this.ifsc}, Intrest Rate : ${this.interestRate}`);
+        
+    }
+    
+}
+
+console.log(axisBank);
+
+
+let hdfcBank = {
+    bankName: "HDFC Bank",
+    location: "Thane",
+    accountNo: "60142245 ",
+    ifsc: "HDFC000567",
+    interestRate: "7.8%",
+    
+    showDetails : function () {
+        console.log(`Name: ${this.bankName}, Location: ${this.location}, Account No.: ${this.accountNo}, IFSC: ${this.ifsc}, Intrest Rate : ${this.interestRate}`);
+        
+    }
+}
+console.log(hdfcBank);
+
+let yesBank = {
+    bankName: "YES  Bank",
+    location: "Airoli",
+    accountNo: "60142245 ",
+    ifsc: "YES000235",
+    interestRate: "8.75%",
+
+    showDetails : function () {
+        console.log(`Name: ${this.bankName}, Location: ${this.location}, Account No.: ${this.accountNo}, IFSC: ${this.ifsc}, Intrest Rate : ${this.interestRate}`);
+        
+    }
+}
+ console.log(yesBank);
+
